3f6dc0402b07aa106201ebdb143ef0f954b5d808
kim <kkhlhj486@naver.com>
2025-09-26 15:25:59 +0900

    1

--- 4주차/sample00.py
+++ 4주차/sample00.py
@@ -1,9 +1,13 @@
 import pandas as pd
 
 covid_file_name = 'C:/Users/kim/Desktop/공부/학교/2-2/빅데이터/25_bigdata/4주차/owid-covid-data.csv'
-raw_pd = pd.read_csv(covid_file_name)
+raw_pd = pd.read_csv(covid_file_name, sep=',')
 
 print(raw_pd)
 #print(dir(raw_pd))
 
+print('*'*50)
 print(raw_pd.info())
+
+print('*'*50)
+print(raw_pd.head())
